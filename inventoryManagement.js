8cac2ab7bf06d64db74fc4a7ff7b98351b5fd2a8
andy <andyaketch@gmail.com>
2026-09-25 18:17:52 +0300

    Functions Lab complete

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -1,5 +1,27 @@
 // Write your code here
+const products = ["Laptop", "Phone", "Headphones", "Monitor"];
 
+
+function logFirstProduct() {
+  console.log(products[0]);
+}
+
+function addProduct(item) {
+  products.push(item);
+}
+
+
+function updateProductName(position, updatedName) {
+if (position >= 0 && position < products.length) {
+    products[position] = updatedName;
+  } else {
+    console.error(`Position ${position} out of bounds`);
+  }
+}
+
+function removeLastProduct() {
+  products.pop();
+}
 
 
 // Export the necessary parts for testing
